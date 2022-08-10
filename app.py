e07e8fd5201aabeaa8916f2a928150a95ab5ca35
mrcabiso <mrcabiso@myseneca.ca>
2022-08-10 20:32:42 +0000

    added environment variable for group name

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ import os
 app = Flask(__name__)
 
 DB_Host = os.environ.get('DB_Host') or "mysql"
-DB_Database = os.environ.get('DB_Database') or "mysqldb"
-DB_User = os.environ.get('DB_User')
-DB_Password = os.environ.get('DB_Password')
+DB_Database = os.environ.get('DB_Database') or "mysql"
+DB_User = os.environ.get('DB_User') or "root"
+DB_Password = os.environ.get('DB_Password') or "paswrd"
+Group_Name = os.environ.get('Group_Name') or "CLO835-Group-8"
 
 @app.route("/")
 def main():
@@ -23,7 +24,7 @@ def main():
         color = '#ff3f3f'
         err_message = str(e)
 
-    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color)
+    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color, Group_Name=Group_Name)
 
 @app.route("/debug")
 def debug():
