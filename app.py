c71351b54bd1483161fa6dc0f694241c0b1f31c2
mrcabiso <mrcabiso@myseneca.ca>
2022-08-12 15:53:02 +0000

    added mysql db deployment manifest

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ DB_Host = os.environ.get('DB_Host') or "mysql"
 DB_Database = os.environ.get('DB_Database') or "mysql"
 DB_User = os.environ.get('DB_User') or "root"
 DB_Password = os.environ.get('DB_Password') 
-clo835Group = os.environ.get('clo835Group') or "paswrd"
+clo835Group = os.environ.get('clo835Group') 
 appurl1 = os.environ.get('appurl1') 
 appurl2 = os.environ.get('appurl2') 
 
